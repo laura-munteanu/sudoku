8e088b8e23a6d134787e856efb0122dec7568b69
Laura Munteanu <laura.tarita@gmail.com>
2020-09-15 23:24:22 +0200

    - selected cells

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { GameState } from './models';
+import { MaterialModule } from './shared/material.module';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   public title = 'sudoku';
   public numberOfBlocks: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   public game: any[][] = [];
   public displayGame: any[][] = [];
   public originalDisplayGame: any[][] = [];
+  public gameCellState: any[][] = [];
   public gameState: GameState;
-
+  public gamePaused: boolean;
+  public blockCellState: string;
+    
 
   ngOnInit() {
     this.initEmptyGame();
@@ -33,6 +39,19 @@ export class AppComponent implements OnInit {
     ];
     this.setDisplayGame();
     this.gameState = GameState.NotStarted;
+
+
+     this.gameCellState =  [
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+    ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  ];
   }
 
   private generate() {
@@ -47,7 +66,6 @@ export class AppComponent implements OnInit {
       [4, 7, 2, 3, 1, 9, 5, 6, 8],
       [8, 6, 3, 7, 4, 5, 2, 1, 9]
     ];
-
 
     this.createGameByLevel();
     this.setDisplayGame();
@@ -67,7 +85,6 @@ export class AppComponent implements OnInit {
       this.game[j][i] = '';
       this.game[j][j] = '';
     }
-
   }
 
 
@@ -78,13 +95,28 @@ export class AppComponent implements OnInit {
       const indexLine = Math.floor(i / 3) * 3;
       const indexColumn = (i % 3) * 3;
       const block = [];
+      const blockCellState = [];
       for (let r = indexLine; r < indexLine + 3; r++) {
         for (let c = indexColumn; c < indexColumn + 3; c++) {
           block.push(this.game[r][c] ? this.game[r][c] : '');
+          //const index = Math.floor(Math.random() * 3);
+          // switch (index) {
+          //   case 0:
+          //     blockCellState.push('normal');
+          //     break;
+          //   case 1:
+          //     blockCellState.push('highlighted');
+          //     break;
+          //   case 2:
+          //     blockCellState.push('selected');
+          //     break;
+          // }
+         // blockCellState.push('normal');
         }
       }
       this.displayGame.push(block);
       this.originalDisplayGame.push(block.slice());
+      //this.gameCellState.push(blockCellState);
     }
     console.log('current game', this.displayGame);
     console.log('original game', this.originalDisplayGame);
@@ -92,6 +124,7 @@ export class AppComponent implements OnInit {
 
 
   public changeGameStatus(buttonName: string) {
+    this.gamePaused = false;
     switch (buttonName) {
       case 'start':
         this.generate();
@@ -99,6 +132,7 @@ export class AppComponent implements OnInit {
         break;
       case 'pause':
         this.gameState = GameState.Paused;
+        this.gamePaused = true;
         break;
       case 'resume':
         this.gameState = GameState.InProgress;
@@ -107,13 +141,31 @@ export class AppComponent implements OnInit {
         this.gameState = GameState.NotStarted;
         break;
     }
+
+    console.log(this.gamePaused)
   }
+
 
 
   public updateGameValues(event, numberBlock, numberCell) {
     this.displayGame[numberBlock][numberCell] = +event.target.value;
     console.log('current game', this.displayGame);
     console.log('original game', this.originalDisplayGame);
+
+  }
+
+
+  public selectCell(numberBlock, numberCell) {
+    for (let i = 0; i < 9; i++){
+      this.gameCellState[numberBlock][i]='highlighted';
+    }
+    this.gameCellState[numberBlock][numberCell]='selected';
+    console.log(numberBlock, numberCell);
+    console.log('selected cell:', this.gameCellState);
+ 
+    this.gameCellState[3][numberCell]='highlighted'
+    console.log('column',this.gameCellState[3][numberCell]);
+
 
   }
 
@@ -126,7 +178,21 @@ export class AppComponent implements OnInit {
   }
 
 
-  // get_row(board, row) {
+
+
+  // this.gameCellState =  [
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  //   ['normal','normal' ,'normal' ,'normal' , 'normal', 'normal','normal' , 'normal','normal'],
+  // ];
+
+  // get_square(board, row) {
   //   // Given a board, we can return a single row
   //   return board[row]
   // }
@@ -141,7 +207,6 @@ export class AppComponent implements OnInit {
   //   return col
   // }
 
-
   // get_square(board) {
   //   let cells = []
   //   for (let r = 0; r < 9; r++) {
@@ -151,9 +216,7 @@ export class AppComponent implements OnInit {
   //   }
   //   return cells
   // }
-  // s = this.get_square(this.game);
-
-
+  // s = this.get_square(this.displayGame);
 
   //generate easy game
   /*start_game(board) {
@@ -170,23 +233,7 @@ export class AppComponent implements OnInit {
     ]
     this.game = board
     let cells = [];
-
-    for (let j = 0; j < 9; j++) {
-      let i = Math.floor(Math.random() * 8);
-      board[j][i] = '';
-      board[i][j] = '';
-      board[i][i] = '';
-      // cells.push(board[j][i])
-    }
-    for (let i = 0; i < 9; i++) {
-      let j = Math.floor(Math.random() * 8);
-      board[i][j] = '';
-      board[j][i] = '';
-      board[j][j] = '';
-      // cells.push(board[i][j])
-    }
     return board
   }
-  y = this.start_game;
   */
 }
---
+++ src/app/components/dialog-body/dialog-body.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-body',
+  templateUrl: './dialog-body.component.html',
+  styleUrls: ['./dialog-body.component.scss']
+})
+export class DialogBodyComponent {
+
+  // constructor(public dialogRef: MatDialogRef<DialogBodyComponent>) { }
+
+  // ngOnInit(): void {
+  // }
+  
+  // close() {
+  //   this.dialogRef.close();
+  // }
+}
